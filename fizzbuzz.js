52fbb740c639b35b7a5075526c22edd4a1851a18
tangmophina <tangmophina@gmail.com>
2026-08-13 15:09:03 +0100

    Implement FizzBuzz code logic in fizzbuzz.js.

---
+++ fizzbuzz.js
@@ -0,0 +1,13 @@
+for (let i = 0; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log(i, "fizzbuzz");
+    }
+    else if (i % 5 === 0) {
+        console.log(i, "fizz");
+    } else if (i % 5 === 0) {
+        console.log(i, "buzz");
+    } else {
+        console.log(i);
+    }
+
+}
